dd0dabacaad7e9fed552966e9171fb75d2c3c5a5
milad pezeshkian <miladpez10@gmail.com>
2025-01-25 10:58:41 +0330

    توضیح درباره تغییرات

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
+import Home from "./Pages/Home/Home.jsx";
 import Research from "./Pages/Research/Research.jsx";
 import Announcements from "./Pages/Announcementts/Announcements.jsx";
 import Publications from "./Pages/Publication/Publications.jsx";
